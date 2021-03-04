fd63474ff1603551f64e7f016d4c593fde6853a1
anuvarghese1395 <anuvarghese1395@gmail.com>
2021-03-04 10:48:08 +0100

    console.log the message

--- src/connection.js
+++ src/connection.js
@@ -4,19 +4,37 @@ import Autobahn from "autobahn";
 class Connection extends React.Component {
   constructor(props) {
     super(props);
+    this.connection = new Autobahn.Connection({
+      url: "ws://testassignment.filmdatabox.com:8104/ws",
+      realm: "democontrol",
+    });
+    this.connection.onopen = this.onOpenConnection;
     this.state = {
       connection: {},
     };
   }
+  componentDidMount = () => {
+    this.connection.open();
+  };
+  componentWillUnmount = () => {
+    this.connection.close();
+  };
+
+  onOpenConnection = (session, details) => {
+    var add2 = function (args) {
+      console.log(args);
+      return args[0] + args[1];
+    };
+    session.register("com.filmdatabox.democontrol.journal", add2);
+
+    session
+      .call("com.filmdatabox.democontrol.journal")
+      .then(function showSum(res) {
+        console.log("sum is", res);
+      }, session.log);
+  };
 
   render() {
-    var connection = new Autobahn.Connection({
-      url: "ws://testassignment.filmdatabox.com:8104/ws",
-      realm: "democontrol",
-    });
-    console.log("connect", connection);
-
-    connection.open();
     return (
       <div>
         <h1>Hello World</h1>
